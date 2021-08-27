c3da2d15855b558f19313abcec77a8d5beb94bf2
Aldo Martinez <aldo.mireles.97@gmail.com>
2021-08-26 19:38:14 -0500

    Remove old ios project and update website with new privacy policy.

--- Pods/Target Support Files/Pods-Nexus ReminderTests/Pods-Nexus ReminderTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nexus_ReminderTests : NSObject
-@end
-@implementation PodsDummy_Pods_Nexus_ReminderTests
-@end
--- Pods/Target Support Files/Pods-Nexus ReminderUITests/Pods-Nexus ReminderUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nexus_ReminderUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Nexus_ReminderUITests
-@end
